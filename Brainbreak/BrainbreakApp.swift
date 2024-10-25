fd93934f94f7f196e9980d8531dec2168dea40e2
Otto <otto.andren@gmail.com>
2024-10-25 13:03:56 +0200

    Initial Commit

---
+++ Brainbreak/BrainbreakApp.swift
@@ -0,0 +1,32 @@
+//
+//  BrainbreakApp.swift
+//  Brainbreak
+//
+//  Created by Otto Andrén on 2024-10-25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BrainbreakApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ BrainbreakTests/BrainbreakTests.swift
@@ -0,0 +1,16 @@
+//
+//  BrainbreakTests.swift
+//  BrainbreakTests
+//
+//  Created by Otto Andrén on 2024-10-25.
+//
+
+import Testing
+
+struct BrainbreakTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
